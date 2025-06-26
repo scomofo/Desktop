6cc75f3cbb4c3ee2be35365f943b69895aa18a5e
scomofo <159295352+scomofo@users.noreply.github.com>
2025-06-26 15:32:25 -0600

    Update email_service.py

--- app/services/email_service.py
+++ app/services/email_service.py
@@ -13,20 +13,4 @@ def send_deal_email_via_sharepoint_service(*args, **kwargs):
     # For now, just log and do nothing.
     pass
 
-# You can add other functions or classes that might be expected by deal_form_view.py
-# For example, if it expects an EmailService class:
-# class EmailService:
-#     def __init__(self, config=None):
-#         self.config = config
-#         logger.info("Dummy EmailService initialized.")
-
-#     def send_deal_email_via_sharepoint(self, *args, **kwargs):
-#         logger.info("Attempted to send deal email via SharePoint service (dummy EmailService class).")
-#         pass
-
-#     # Add any other methods that deal_form_view.py might try to call
-#     def some_other_email_function(self, *args, **kwargs):
-#         logger.info("Called some_other_email_function (dummy).")
-#         pass
-
 logger.info("Dummy email_service.py loaded.")
